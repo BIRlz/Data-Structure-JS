ccb0ca281042f8aafa9b4b31a366489449f3a4d2
SayGood-bye <BIRlz@users.noreply.github.com>
2017-05-10 13:08:25 +0800

    Create Stack.js

--- Stack/Stack.js
+++ Stack/Stack.js
@@ -1,28 +1,61 @@
+//数组实现
 function Stack(){
-    this.dataStack = [];
-    this.pop = 0;//Memorize the top of the Stack and length
+   this.items = [];
+   this.size = 0;
 }
-//Go into the Stack
-Stack.prototype.push = function(ele){
-    this.dataStack[this.pop++]=ele; 
+Stack.prototype = {
+   constructor:Stack,
+   push:function(data){
+       this.items[this.size++] = data;
+   },
+   pop:function(){
+       return this.items[--this.size];
+   },
+   clear:function(){
+       this.size = 0;
+       this.items = [];
+   },
+   perk:function(){
+       return this.items[this.size-1];
+   }
 }
-
-//Out of the Stack
-Stack.prototype.pop = function(){
-    return this.dataStack[--this.pop];
-}
-
-//Clear
-Stack.prototype.clear = function(){
-    this.pop = 0;
-}
-
-//Length
-Stack.prototype.getLength = function(){
-    return this.top;
-}
-
-//retur the peep of the Stack
-Stack.prototype.peep = function(){
-    return this.dataStack[top-1];
-}
+//链表实现
+    function Stack(){
+        this.top = null;
+        this.size = 0;
+    }
+    Stack.prototype = {
+        constructor:Stack,
+        push:function(data){
+            var node = {
+                data:data,
+                next:null
+            };
+            node.next = this.top;
+            this.top = node;
+            this.size++;
+        },
+        pop:function(){
+            if(this.top === null){
+                return null;
+            }
+            var out = this.top;
+            this.top = this.top.next;
+            if(this.size > 0){
+                this.size--;    
+            }
+            return out.data;
+        },
+        perk:function(){
+            return this.top === null ? null:this.top.data; 
+        },
+        clear:function(){
+            this.top = null;
+            this.size = 0;
+        }
+var stack = new Stack();
+stack.push('k');
+stack.push('b');
+console.log(stack.perk());//输出b
+stack.pop();
+console.log(stack.perk());//输出k
